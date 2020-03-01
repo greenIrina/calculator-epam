6027d121f92cd1f5080d2cf50cb6c4768d7fb976
Nik Carlson <gleb_fedorov_00@mail.ru>
2020-03-02 00:23:32 +0300

    уг
    Я чёт хз как это всё сделать.
    Распарсить строку: ParserService.evaluate(expression)
    
    Всё ещё доброй ночи

--- src/main/java/com/linicar/calculator/controller/SimpleCalcPage.java
+++ src/main/java/com/linicar/calculator/controller/SimpleCalcPage.java
@@ -1,13 +1,29 @@
 package com.linicar.calculator.controller;
 
 
+import com.linicar.calculator.service.ParserService;
+import com.linicar.calculator.service.ParserServiceImpl.exceptions.EvaluatingExceptions;
+import com.linicar.calculator.service.ParserServiceImpl.exceptions.ParserException;
+import com.linicar.calculator.service.ParserServiceImpl.exceptions.UnsupportedModeException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+//получает строку-кидает её в парсер
+//получает сроку от парсера-кидает её в ответ
 public class SimpleCalcPage extends Page {
+    private static String mode = "i";
+    private static ParserService parserService;
 
+    public SimpleCalcPage() throws UnsupportedModeException {//этого здесь быть не должно
+        parserService = new ParserService(mode);
+    }
 
-    //получает строку-кидает её в парсер
-    //получает сроку от парсера-кидает её в ответ
+    @RequestMapping(value = "/commons", method = RequestMethod.GET)
+    public String greetingForm(String expression) throws EvaluatingExceptions, ParserException {
+        return "commons";
+    }
 
 }
