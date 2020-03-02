e0cb76d823507118cf9f4344e95fa211b709c02e
Irina <45588607+greenIrina@users.noreply.github.com>
2020-03-02 23:34:12 +0300

    update

--- src/main/java/com/linicar/calculator/parser/expression/exceptions/UnknownOpException.java
+++
@@ -1,7 +0,0 @@
-package com.linicar.calculator.parser.expression.exceptions;
-
-public class UnknownOpException extends ParsingException {
-    public UnknownOpException(final String id, final int ind) {
-        super(String.format("Unknown identifier '%s' at position: %d", id, ind));
-    }
-}
