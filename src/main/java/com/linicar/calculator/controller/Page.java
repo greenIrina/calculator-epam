21edd106d98ece293d86ef5b6611955d0552419b
Irina <45588607+greenIrina@users.noreply.github.com>
2020-03-05 10:16:32 +0300

    add EngineeringCalcPage

--- src/main/java/com/linicar/calculator/controller/Page.java
+++ src/main/java/com/linicar/calculator/controller/Page.java
@@ -1,5 +1,8 @@
 package com.linicar.calculator.controller;
 
+import com.linicar.calculator.repository.ParserRepository;
+import com.linicar.calculator.service.ParserServiceImpl.exceptions.EvaluatingExceptions;
+import com.linicar.calculator.service.ParserServiceImpl.exceptions.ParserException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -16,6 +19,19 @@ public class Page {
 
     public void putMessage(HttpSession httpSession, String message) {
         httpSession.setAttribute("message", message);
+    }
+
+    void calculation(String expr, HttpSession httpSession, ParserRepository parserRepository) {
+        try {
+            String ans = parserRepository.evaluate(expr).toString();
+            if (ans.length() >= 2 && ans.substring(ans.length() - 2, ans.length()).equals(".0")) {
+                putMessage(httpSession, "Result: " + ans.substring(0, ans.length() - 2));
+            } else {
+                putMessage(httpSession, "Result: " + ans);
+            }
+        } catch (ParserException | EvaluatingExceptions e) {
+            putMessage(httpSession, "Error: " + e.getMessage());
+        }
     }
 
 }
--- src/main/java/com/linicar/calculator/controller/SimpleCalcPage.java
+++ src/main/java/com/linicar/calculator/controller/SimpleCalcPage.java
@@ -32,16 +32,7 @@ public class SimpleCalcPage extends Page {
     @PostMapping("base")
     public String calc(@Valid @ModelAttribute("expression") String expression, HttpSession httpSession)
             throws EvaluatingExceptions {
-        try {
-            String ans = simpleCalcService.evaluate(expression).toString();
-            if (ans.substring(ans.length() - 2, ans.length()).equals(".0")) {
-                putMessage(httpSession, "Result: " + ans.substring(0, ans.length() - 2));
-            } else {
-                putMessage(httpSession, "Result: " + ans);
-            }
-        } catch (ParserException e) {
-            putMessage(httpSession, "Error: " + e.getMessage());
-        }
+        calculation(expression, httpSession, simpleCalcService);
         return "simpleCalc";
     }
 }
